9c976d9bef7ae6c6a08ddfb267e48d4d2f907301
hanbar <h.barvikova@centrum.cz>
2021-05-21 15:50:46 +0200

    že by? :)

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { HashRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import Header from './header';
 import Main from './mainContent';
 import Footer from './footer';
@@ -6,7 +6,7 @@ import Footer from './footer';
 const App = () => {
 
     return (
-        <Router>
+        <Router basename={process.env.NODE_ENV === 'development' ? '/' : '/hyposka'}>
             <Header className="App-header" />
             <Main/>
             <Footer/>
